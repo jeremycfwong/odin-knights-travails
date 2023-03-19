77333d2efe77c023d17a8022560814362e3f2760
jeremycfwong <71006547+jeremycfwong@users.noreply.github.com>
2023-03-19 13:53:05 +0800

    Add better naming
    As title

--- knightTravails.js
+++ knightTravails.js
@@ -1,65 +1,72 @@
-const board = new Map();
+// Historical record of all moves and their previous moves
+const registeredMoves = new Map();
 
+// Attached to a move is the ability to:
+    // view/set their previous move (Parent list)
+    // generate possible move （to be Queued)
+    // fetch from registered moves if exist (Visted list)
 const move = (x,y) => {
-    let previousMove;
+    let prev;
+    const getPrev = () => prev;
+    const setPrev = (newPrev) => {prev ||= newPrev};
 
-    const MOVESET = [ 
-                    [ 1,  2], [ 2, 1],
-                    [-1,  2], [-2, 1],
-                    [ 1, -2], [ 2,-1],
-                    [-1, -2], [-2,-1]
-                    ];
-    
-    const getPrev = () => previousMove;
-    const setPrev = (newPrev) => {previousMove ||= newPrev}
+    const MOVESET = [
+        [1, 2], [2, 1],
+        [-1,2], [-2,1],
+        [1,-2], [2,-1],
+        [-1,-2], [-2,-1]
+    ]
 
     const name = () => `[${x}, ${y}]`
-    const createKnightMoves = () => {
-        return MOVESET.map(newSquareGen).filter(Boolean);
+
+    const createMoves = () => {
+        return MOVESET.map(generateMoves).filter(Boolean)
     }
 
-    const newSquareGen = ([xOffset, yOffset]) => {
+    const generateMoves = ([xOffset, yOffset]) => {
         const [newX, newY] = [x + xOffset, y + yOffset];
-        if (0 <= newX && newX < 8 && 0<= newY && newY <8) {
-            return move(newX,newY)
+        if(0 <= newX && newX < 8 && 0 <= newY && newY < 8){
+            return move(newX, newY)
         }
     }
 
-    if(board.has(name())) {
-        return board.get(name());
+    if (registeredMoves.has(name())) {
+        return registeredMoves.get(name())
     } else {
-        const newSquare = {name, getPrev, setPrev, createKnightMoves}
-        board.set(name(), newSquare)
-        return newSquare
+        const newMove = {name,createMoves,getPrev,setPrev}
+        registeredMoves.set(name(), newMove)
+        return newMove
     }
 }
 
+// Function to create two moves
+// Look thorugh all possibility from final to reach beginning
+    // Queue all possibility if haven't found whilst marking their prev
+// With all previous moves recorded, extract the fastest path from beginning -> end from the last prev record
+function knightTravails(start,end){
+    registeredMoves.clear();
 
+    const beginning = move(...start);
+    const final = move(...end);
 
-function knightTravails(start, end){
-    board.clear();
+    const queue = [final];
+    while(!queue.includes(beginning)){
+        const current = queue.shift();
 
-    const from = move(...start);
-    const target = move(...end);
-
-    const queue = [target];
-    while(!queue.includes(from)) {
-        const current = queue.shift()
-
-        const nextList = current.createKnightMoves();
-        nextList.forEach(element => element.setPrev(current));
-        queue.push(...nextList);
+        const nextMove = current.createMoves();
+        nextMove.forEach(element => element.setPrev(current))
+        queue.push(...nextMove);
     }
 
-    const path = [from]
-    while(!path.includes(target)){
-        const next = path.at(-1).getPrev();
+    const path = [beginning]
+    while(!path.includes(final)){
+        const next = path.at(-1).getPrev()
         path.push(next)
     }
 
-    console.log(`The shortest path was ${path.length - 1} moves!`);
-    console.log("The moves were:");
-    path.forEach(square => console.log(square.name()));
+    console.log(`The shortest path was ${path.length - 1} moves!`)
+    console.log('The moves were:')
+    path.forEach((node) => console.log(node.name()))
 }
 
-knightTravails([1,1],[7,6]);
+knightTravails([1,1],[3,4])
